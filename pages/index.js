3193d642652a6accc3ace7951261db46c1106a8f
William Duplenne <will.dupl95@gmail.com>
2020-01-31 21:44:09 +0100

    :art: Add paragraph (#1)

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Head from '../components/head';
-import Nav from '../components/nav';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Head from "../components/head";
+import Nav from "../components/nav";
 
 const Home = () => {
   const [date, setDate] = useState(null);
 
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.json();
       setDate(newDate);
     }
@@ -22,13 +22,14 @@ const Home = () => {
 
       <div className="hero">
         <h1 className="title">Welcome to Next!</h1>
+        <p>Testing new build workflow</p>
         <p className="description">
-          To get started, edit the <code>pages/index.js</code> or{' '}
+          To get started, edit the <code>pages/index.js</code> or{" "}
           <code>pages/api/date.js</code> files, then save to reload.
         </p>
 
         <p className="row date">
-          The date is:&nbsp;{' '}
+          The date is:&nbsp;{" "}
           {date ? (
             <span>
               <b>{date.date}</b>
